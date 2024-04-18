46db8c79a362b90c30f8fb11bc93e928e4573268
bakurkokhodze <b.kokhodze@optimogroup.io>
2024-04-18 17:03:19 +0400

    layouts for admin

--- app/(main)/layout.tsx
+++
@@ -1,9 +0,0 @@
-export default function RootLayout({children,}: Readonly<{ children: React.ReactNode; }>) {
-  return (
-      <section className={"bg-red-800"}>
-        <p>layout main header</p>
-        {children}
-        <p>layout main footer</p>
-      </section>
-  );
-}
