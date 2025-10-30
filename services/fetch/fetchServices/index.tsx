458a985e4351e66ba55be53aa4043f10172f4fad
bakurkokhodze <b.kokhodze@optimogroup.io>
2025-10-30 13:19:20 +0400

    fixes from bakuri

--- services/fetch/fetchServices/index.tsx
+++ services/fetch/fetchServices/index.tsx
@@ -1,12 +1,13 @@
-import { IFilter } from "@/@types/IServices";
-import { axiosWithAuth } from "@/configs/axios";
-import { notification } from "antd";
+import {IFilter} from "@/@types/IServices";
+import {axiosWithAuth} from "@/configs/axios";
+import {notification} from "antd";
 
 const BASEAPI = process.env.NEXT_PUBLIC_API_URL;
 
 export const fetchServices = async (filter: IFilter) => {
   try {
     const params: any = {};
+    params.status = "all"
     if (filter.slug) params.slug = filter.slug;
     if (filter.pageNumber) params.page = filter.pageNumber;
     if (filter.pageSize) params.limit = filter.pageSize;
@@ -18,7 +19,7 @@ export const fetchServices = async (filter: IFilter) => {
       params.categoryId = (filter as any).categoryId;
     if ((filter as any).status) params.status = (filter as any).status;
 
-    const { data } = await axiosWithAuth.get(`${BASEAPI}/services`, {
+    const {data} = await axiosWithAuth.get(`${BASEAPI}/services`, {
       params,
     });
     return data;
